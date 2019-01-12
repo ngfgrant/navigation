246cf0c45fc95297eba5913b3cdaffdf9fabc4e1
Niall Grant <ngfgrant@gmail.com>
2019-01-12 13:54:50 +0000

    Add __init__.py to navigation

---
+++ navigation/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+
+__author__ = 'Niall Grant'
+__email__ = 'ngfgrant@gmail.com'
+__version__ = '0.1.0'
+
+
+# Set up logging to ``/dev/null`` like a library is supposed to.
+# http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
+class NullHandler(logging.Handler):  # pragma: no cover
+    def emit(self, record):
+        pass
+
+
+logging.getLogger('navigation').addHandler(NullHandler())
